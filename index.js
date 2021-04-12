ce1aa3a6ec980469c3b98eaa2a6eed70d0c02704
Martin Cermak <crudo@crudo.cz>
2021-04-12 21:35:47 +0200

    Response validator not working

--- index.js
+++ index.js
@@ -3,14 +3,48 @@ const OpenApiValidator = require("express-openapi-validator");
 
 const app = new express();
 
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: false }));
+
 app.use(
   OpenApiValidator.middleware({
-    apiSpec: "./index.json",
-    validateRequests: true, // (default)
-    validateResponses: true, // false by default
+    apiSpec: "./index.yaml",
+    validateRequests: true,
+    validateResponses: true,
   })
 );
 
+app.get("/clusters", (req, res) => {
+  // should be array, not { data: ARRAY }
+  const data = { data: [{ cluster_name: "Alpha" }, { cluster_name: "Beta" }] };
+  res.json(data);
+});
+
+app.get("/clusters/:clusterId", (req, res) => {
+  let data = {};
+
+  console.log("🔔  req.params", req.params);
+
+  // fake only one cluster
+  if (req.params.clusterId === "1") {
+    // missing required cluster_id
+    data = { cluster_name: "Alpha" };
+
+    res.json(data);
+  } else {
+    res.json({ error: 404 });
+  }
+});
+
+app.use((err, req, res, next) => {
+  // format error
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
+});
+
 app.listen(3333, () => {
-  console.log(`Example app listening at http://localhost:3333`);
+  console.log(`\nExample app listening at http://localhost:3333`);
 });
